e1a48d5fa20f5778ec8a05aa508a618623eae6fe
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-02-16 00:34:27 -0300

    creating setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from shutil import copyfile
+from platform import system as platform
+from distutils.core import setup
+
+setup(
+    name='PySchema',
+    version='1.0',
+    packages=['PySchema'],
+    requirements=[],
+    url='https://github.com/mateusmoutinho/PySchema',
+    license='MIT',
+    author='Mateus Moutinho',
+    author_email='mateusmoutinho01@gmial.com',
+    description=''
+)
+
