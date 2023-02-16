395e08195c48edc99611bf8ab9ac8634b58dcd8d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-02-16 00:21:26 -0300

    at

--- main_test.py
+++ main_test.py
@@ -1,6 +1,6 @@
 import unittest
 
 from testing.testing_treat_and_get_any import TestTreatAndGetAny
-
+from testing.testing_treat_and_get_numbers import TestTreatAndGetNumbers
 if __name__ == '__main__':
     unittest.main()
